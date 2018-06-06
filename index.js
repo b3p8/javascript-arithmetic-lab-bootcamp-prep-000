cb92a9d82814de531d5eccb020536aa56d143826
Brandon Pate <bepate@att.net>
2018-06-06 18:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function inc(n) {
 }
 
 function dec(n) {
-  return ((n--)-1);
+  return ((n--) - 1);
 }
 
 function makeInt(n) {
@@ -27,5 +27,5 @@ function makeInt(n) {
 }
 
 function makeInt() {
-  parseInt(n).toString(0);
+  multiply('0 * 2328');
 }
